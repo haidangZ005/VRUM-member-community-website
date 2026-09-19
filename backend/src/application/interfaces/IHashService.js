4455f2b7a975a325574c0a905f8cc59a57261503
haidangZ005 <179605896+haidangZ005@users.noreply.github.com>
2026-09-19 09:40:28 +0700

    refactor(ports): remove unused hash service stub

--- backend/src/application/interfaces/IHashService.js
+++
@@ -1,7 +0,0 @@
-class IHashService {
-  async hash(_value) { throw new Error('Not implemented'); }
-  async compare(_value, _hash) { throw new Error('Not implemented'); }
-}
-
-module.exports = IHashService;
-
