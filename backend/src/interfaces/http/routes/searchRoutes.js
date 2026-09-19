91f5c86949f6d88248f455402a5a4eea392f8f85
haidangZ005 <179605896+haidangZ005@users.noreply.github.com>
2026-09-19 09:39:39 +0700

    refactor(search): use native Express async error handling

--- backend/src/interfaces/http/routes/searchRoutes.js
+++ backend/src/interfaces/http/routes/searchRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const asyncHandler = require('../../../shared/utils/asyncHandler');
 const validateRequest = require('../middlewares/validateRequest');
 const { searchSchema } = require('../validators/searchValidator');
 
 function makeSearchRoutes(controller, optionalAuthMiddleware) {
   const router = express.Router();
-  router.get('/', optionalAuthMiddleware, validateRequest(searchSchema, 'query'), asyncHandler(controller.search));
+  router.get('/', optionalAuthMiddleware, validateRequest(searchSchema, 'query'), controller.search);
   return router;
 }
 
