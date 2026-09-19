a707fd66dccb5a788b0c1e557890dd465b3119f9
haidangZ005 <179605896+haidangZ005@users.noreply.github.com>
2026-09-19 09:40:57 +0700

    refactor(ports): remove unused refresh token repository stub

--- backend/src/domain/repositories/IRefreshTokenRepository.js
+++
@@ -1,9 +0,0 @@
-class IRefreshTokenRepository {
-  async create(_token) { throw new Error('Not implemented'); }
-  async findValidByHash(_tokenHash) { throw new Error('Not implemented'); }
-  async revokeByHash(_tokenHash) { throw new Error('Not implemented'); }
-  async revokeAllForUser(_userId) { throw new Error('Not implemented'); }
-}
-
-module.exports = IRefreshTokenRepository;
-
