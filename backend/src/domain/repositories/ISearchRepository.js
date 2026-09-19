e76af5f93f38b7c2fac8db9dacd498693182ca8a
haidangZ005 <179605896+haidangZ005@users.noreply.github.com>
2026-09-19 09:41:00 +0700

    refactor(ports): remove unused search repository stub

--- backend/src/domain/repositories/ISearchRepository.js
+++
@@ -1,9 +0,0 @@
-class ISearchRepository {
-  async searchPosts(_filters) { throw new Error('Not implemented'); }
-  async searchComments(_filters) { throw new Error('Not implemented'); }
-  async searchCommunities(_filters) { throw new Error('Not implemented'); }
-  async searchUsers(_filters) { throw new Error('Not implemented'); }
-  async searchMedia(_filters) { throw new Error('Not implemented'); }
-}
-
-module.exports = ISearchRepository;
