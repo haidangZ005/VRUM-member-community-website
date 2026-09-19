12a8d1fc85df2b119dbf39d6fdc9fad7cd793c6e
haidangZ005 <179605896+haidangZ005@users.noreply.github.com>
2026-09-19 09:41:07 +0700

    refactor(ports): remove unused vote repository stub

--- backend/src/domain/repositories/IVoteRepository.js
+++
@@ -1,6 +0,0 @@
-class IVoteRepository {
-  async setPostVote(_postId, _userId, _value) { throw new Error('Not implemented'); }
-  async setCommentVote(_commentId, _userId, _value) { throw new Error('Not implemented'); }
-}
-
-module.exports = IVoteRepository;
